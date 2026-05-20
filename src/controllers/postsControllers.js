8f9f13450cf4060eaba95a080b0365e64421bebc
Ranz00 <ranz00@users.noreply.github.com>
2026-05-19 23:05:06 -0300

    refactor(controllers): add async/await to posts controllers

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,37 +1,37 @@
 // src/controllers/postsControllers.js
-// Controladores HTTP para posts. Incluye filtro por author.
+// Controladores HTTP para posts con async/await
 
 import * as postsService from '../services/postsServices.js'
 
-export const getAll = (req, res) => {
-  const posts = postsService.getAll()
+export const getAll = async (req, res) => {
+  const posts = await postsService.getAll()
   res.json(posts)
 }
 
-export const getById = (req, res) => {
-  const post = postsService.getById(Number(req.params.id))
+export const getById = async (req, res) => {
+  const post = await postsService.getById(Number(req.params.id))
   if (!post) return res.status(404).json({ error: 'Post no encontrado' })
   res.json(post)
 }
 
-export const getByAuthor = (req, res) => {
-  const posts = postsService.getByAuthor(Number(req.params.authorId))
+export const getByAuthor = async (req, res) => {
+  const posts = await postsService.getByAuthor(Number(req.params.authorId))
   res.json(posts)
 }
 
-export const create = (req, res) => {
-  const post = postsService.create(req.body)
+export const create = async (req, res) => {
+  const post = await postsService.create(req.body)
   res.status(201).json(post)
 }
 
-export const update = (req, res) => {
-  const post = postsService.update(Number(req.params.id), req.body)
+export const update = async (req, res) => {
+  const post = await postsService.update(Number(req.params.id), req.body)
   if (!post) return res.status(404).json({ error: 'Post no encontrado' })
   res.json(post)
 }
 
-export const remove = (req, res) => {
-  const deleted = postsService.remove(Number(req.params.id))
+export const remove = async (req, res) => {
+  const deleted = await postsService.remove(Number(req.params.id))
   if (!deleted) return res.status(404).json({ error: 'Post no encontrado' })
   res.status(204).send()
 }
